7cf6e32ae4199532d0c99a1e12f58f347a959b26
Channing Pear <cpear@mac.com>
2015-02-14 01:51:49 -0600

    added saving

--- background.js
+++ background.js
@@ -2,9 +2,22 @@ var x = 0
 var y = 0
 var xDifference = 0;
 var yDifference = 0;
-var distance = 0;
+var distance = loadDistance();
 var oldX = 0;
 var oldY = 0;
+
+function loadDistance() {
+    var dist = parseInt(localStorage["distance"]);
+    console.log(dist)
+    if (dist == undefined) {
+        dist = 0;
+    }
+    return dist;
+}
+
+function saveDistance(dist) {
+    localStorage["distance"] = dist;
+}
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -18,7 +31,8 @@ chrome.runtime.onMessage.addListener(
             y = request.coords.y;
             xDifference = x - oldX;
             yDifference = y - oldY;
-            distance += Math.sqrt(Math.pow(xDifference,2) + Math.pow(yDifference,2));
+            distance += Math.floor(Math.sqrt(Math.pow(xDifference,2) + Math.pow(yDifference,2)));
+            saveDistance(distance);
             sendResponse({farewell: "success"});
         }
 });
