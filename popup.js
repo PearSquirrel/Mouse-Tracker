88ec8e2fab27b68c63f92b8919f8edb49abae179
Sumanth Balaraman <Sumanth@wireless-10-147-173-58.public.utexas.edu>
2015-02-13 19:53:02 -0600

    basic messaging to background script

--- popup.js
+++ popup.js
@@ -5,15 +5,6 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.greeting == "hello")
-            sendResponse({farewell: "goodbye"});
-    });
-
 document.onmousemove = function(e)
 {
     x = e.pageX;
