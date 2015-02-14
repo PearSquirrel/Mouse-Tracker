8138ccc18c096b9d2b97bf043b942c838ced6a1a
Channing Pear <PearSquirrel@users.noreply.github.com>
2015-02-13 20:59:55 -0600

    get out my house

--- channing.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $('#headline').text('SCRUB');
-    $('.brand img').attr('src', 'http://www.clker.com/cliparts/9/5/y/2/G/v/cartoon-squirrel-hi.png');
-    //http://globaltoynews.typepad.com/.a/6a0133ec87bd6d970b0134807e9e3f970c-200wi
-});
-
-headline
