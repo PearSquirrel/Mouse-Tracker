b161002c6d4007f67d904c91d4d3c90dbf30248d
Sumanth Balaraman <Sumanth@wireless-10-147-173-58.public.utexas.edu>
2015-02-13 20:44:08 -0600

    activates insance optimizations to channingpear.com

--- page_script.js
+++ page_script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     //$('#gbqfq').css('color', 'blue');
     
     document.onmousemove = function(e) {
-        $('#gbqfq').val(e.pageX + ', ' + e.pageY);
+        //$('#gbqfq').val(e.pageX + ', ' + e.pageY);
         
         chrome.runtime.sendMessage({greeting: "hello", coords: {x: e.pageX, y: e.pageY}}, function(response) {
             console.log(response.farewell);
