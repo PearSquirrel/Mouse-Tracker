a5d8d26608382b9617388ab833bf59919ba71ec3
Channing Pear <cpear@mac.com>
2015-02-13 20:55:43 -0600

    cleaned up sumanth's mess

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 var distance = 0;
+var x = 0;
+var y = 0;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
-        if (request.greeting == "hello") {
+        if (request.greeting == "coords") {
+            // new x and y are request.coords.x and request.coords.y
             distance += 1;
             sendResponse({farewell: "success"});
-        }
-        else if(request.greeting == "gimme_mah_info_brej") {
-            sendResponse({farewell: "distance", distance: distance});
         }
 });
--- popup.js
+++ popup.js
@@ -13,11 +13,9 @@ function renderStatus(statusText) {
     //document.getElementById('status').textContent = statusText;
 };
 
-/*document.onmousemove = function(e) {
-    x = e.pageX;
-    y = e.pageY;
-    renderStatus('x:' + x + ', y:' + y);
-};*/
+document.onmousemove = function(e) {
+    //renderStatus('x:' + x + ', y:' + y);
+};
 
 /*chrome.runtime.sendMessage({greeting: "gimme_mah_info_brej"}, function(response) {
     console.log(response.farewell);
