b1f15123a66ea1fc069e15bfd6757f1de01b7a8a
Sumanth Balaraman <Sumanth@wireless-10-147-173-58.public.utexas.edu>
2015-02-13 19:51:26 -0600

    started adding messaging and created background script

--- page_script.js
+++ page_script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $('body').css('background-color', 'yellow');
+    
+    console.log('document ready');
+    //$('body').css('background-color', 'yellow');
+    //$('#gbqfq').css('color', 'blue');
+    
+    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+        console.log(response.farewell);
+    });
+    
+    document.onmousemove = function(e) {
+        $('#gbqfq').val(e.pageX + ', ' + e.pageY);
+        
+    };
 });
--- popup.js
+++ popup.js
@@ -5,6 +5,15 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.greeting == "hello")
+            sendResponse({farewell: "goodbye"});
+    });
+
 document.onmousemove = function(e)
 {
     x = e.pageX;
