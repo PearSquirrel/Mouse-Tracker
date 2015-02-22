dd29f92519687956d6aab0601a4b19afaafa3103
Sumanth Balaraman <Sumanth@wireless-10-148-82-200.public.utexas.edu>
2015-02-21 22:23:58 -0600

    added distance stuff and appropriate tab names

--- popup.js
+++ popup.js
@@ -1,10 +1,28 @@
 var bgPage = chrome.extension.getBackgroundPage();
 
+var ppi = 227.0; // need to change based on user screen size
+var INCHES = 0;
+var FEET = 1;
+var MILES = 2;
+
 $(document).ready(function() {
-    $('#status').text('initial');
-    renderStatus("Distance: " + bgPage.distance);
+    //renderStatus("hello");
+    var status = "Pixels: " + bgPage.distance + "\n...";
+    var conversions = convertFromPixels(bgPage.distance);
+    status += "\nInches: " + conversions[INCHES];
+    status += "\nFeet: " + conversions[FEET];
+    status += "\nMiles: " + conversions[MILES];
+    renderStatus(status);
 });
 
 function renderStatus(statusText) {
     $('#status').text(statusText);
+};
+
+function convertFromPixels(pixels) {
+    var conversions = [];
+    conversions.push(pixels / ppi); // inches
+    conversions.push(conversions[INCHES] / 12.0); // feet
+    conversions.push(conversions[FEET] / 5280.0); // miles
+    return conversions;
 };
