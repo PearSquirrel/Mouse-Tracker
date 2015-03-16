f22ad23ab3da0203c0f2274fd0ef7d3673b62559
Rohit Datta <rohitdatta11@gmail.com>
2015-03-15 23:05:19 -0400

    Revert "Revert "added comments in popup.js""
    This reverts commit 8f2e1d2fb458bdcfb587c7f2dc921a06cd004f92.
    
    did not fix previous issue. reverting back to previous

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,3 +1,4 @@
+// retrieve the background page, background.js
 var bgPage = chrome.extension.getBackgroundPage();
 
 var ppi = 227.0; // need to change based on user screen size
@@ -12,21 +13,29 @@ var pixelsCutoff = 100000;
 var inchesCutoff = 1000;
 var feetCutoff = 5280;
 
+// true if the quick view header is showing the daily distance
 var showingTodayStat = true;
+
+// the quick view header
 var $distanceTraveled;
 
+// variables containing the daily and all time distances retrieved from the background page
 var pixels_allTimeDistance;
 var pixels_dailyDistance;
+
+// lists containing the converted values for the daily and all time distances
 var dailyConversions;
 var allTimeConversions; 
 
+// the two tables containing the daily and all time distances in different units
 var $allTimeDistanceTable;
 var $dailyDistanceTable;
 
 $(document).ready(function() {
+    
+    // for the tabbing effect
     var pages = document.querySelector('core-pages');
     var tabs = document.querySelector('paper-tabs');
-    
     tabs.addEventListener('core-select', function() {
         pages.selected = tabs.selected;
     });
@@ -34,6 +43,7 @@ $(document).ready(function() {
     $allTimeDistanceTable = $('#allTimeDistanceTable');
     $dailyDistanceTable = $('#dailyDistanceTable');
     
+    // retrieves the daily and all time distances from the background page, background.js
     pixels_allTimeDistance = bgPage.allTimeDistance;
     pixels_dailyDistance = bgPage.dailyDistance;
     
@@ -54,18 +64,27 @@ $(document).ready(function() {
     // $('.tab-content').append('Current Time: ' + bgPage.currentMinutes);
 
     $distanceTraveled = $('#distance-traveled');
+    
+    // sets the initial text of the quick view header to be the daily distance
     $distanceTraveled.html(generateQuickViewNumber(pixels_dailyDistance, dailyConversions) + ' today');
     
+    // activates the first pulse/switch after 1 second
     setTimeout(updateQuickView, 1000);
+    
+    // activates the consecutive pulses/switches every 8 seconds
+    // the 8 seconds includes the 3 seconds it takes to complete the switching animation
     setInterval(updateQuickView, 8000); 
 });
  
+// switches between showing the daily amount and the all time amount
 var updateQuickView = function() {
     $distanceTraveled.animate({'opacity': 0}, 1500, function() {
         if(showingTodayStat) {
+            // switches to all time distance
             $(this).html(generateQuickViewNumber(pixels_allTimeDistance, allTimeConversions) + ' total');
         } 
         else {
+            // switches to today/daily distance
             $(this).html(generateQuickViewNumber(pixels_dailyDistance, dailyConversions) + ' today');
         }
         showingTodayStat = !showingTodayStat; 
@@ -73,6 +92,7 @@ var updateQuickView = function() {
     }); 
 };
 
+// based on the distances, this function determines which unit to display the 
 function generateQuickViewNumber(pixels_dailyDistance, dailyConversions) {
     var number = "";
     if (pixels_dailyDistance < pixelsCutoff) {
@@ -90,17 +110,19 @@ function generateQuickViewNumber(pixels_dailyDistance, dailyConversions) {
     return number;
 }
 
-
+// adds a row to the table containing the stats for all time distance
 function addAllTimeDistanceItem(units, distance) {
     var tableAddition = '<tr class="distanceTableRow"><td>' + units + '</td><td>' + distance + '</td></tr>';
     $allTimeDistanceTable.append(tableAddition);
 }
 
+// adds a row to the table containing the stats for daily distance
 function addDailyDistanceItem(units, distance) {
     var tableAddition = '<tr class="distanceTableRow"><td>' + units + '</td><td>' + distance + '</td></tr>';
     $dailyDistanceTable.append(tableAddition);
 }
 
+// returns a list containing values for different units (inches, feet, and miles) converted from pixels
 function convertFromPixels(pixels) {
     var conversions = [];
     conversions.push( (Math.round(pixels / ppi * 100000) / 100000) ); // inches
